f6d47ede21cec7445a21bfc62766810fd2768a4c
ArtthSilva <arthur12pereira1@gmail.com>
2023-09-03 22:47:41 -0300

    feat: add exercicio média de notas de aluno

---
+++ src/NotasMedia/Aluno.java
@@ -0,0 +1,26 @@
+package NotasMedia;
+
+public class Aluno {
+    String nome;
+    double nota;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+
+    public Aluno() {
+    }
+}
---
+++ src/NotasMedia/Main.java
@@ -0,0 +1,27 @@
+package NotasMedia;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Aluno aluno = new Aluno();
+        double soma = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("informe o nome");
+        String nome = scanner.nextLine();
+        aluno.setNome(nome);
+
+        System.out.println("quantas notas?");
+        int qntNotas = scanner.nextInt();
+
+        for (int i = 1; i < qntNotas + 1; i++){
+            System.out.println("nota "+i);
+            double notas = scanner.nextDouble();
+            aluno.setNota(notas);
+            soma += aluno.getNota();
+        }
+        double result = soma / qntNotas;
+        System.out.println("media: "+result+" \naluno: "+aluno.getNome());
+    }
+}
